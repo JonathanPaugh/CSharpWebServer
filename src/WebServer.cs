82e893d0170bf3d0fef1ecf757223f53f14a818d
Jonathan Paugh <j.d.paugh@gmail.com>
2021-11-29 20:23:06 -0800

    Template Test

--- src/WebServer.cs
+++ src/WebServer.cs
@@ -1,9 +1,16 @@
-﻿using System.IO;
+﻿using System.Threading.Tasks;
+using JapeHttp;
+using JapeWeb;
 
 namespace CSharpWebServer
 {
     public class WebServer : JapeWeb.WebServer
     {
-        public WebServer(int http, int https) : base(http, https) {}
+        private readonly Templater templater;
+
+        public WebServer(int http, int https) : base(http, https)
+        {
+            templater = CreateTemplater("./template");
+        }
     }
 }
