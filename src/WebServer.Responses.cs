33c16ff6f2a1758654af5d671698331621cd1731
Jonathan Paugh <j.d.paugh@gmail.com>
2021-12-04 09:58:22 -0800

    Client Request Test -Client test to make a request to the server to insert into mongo

---
+++ src/WebServer.Responses.cs
@@ -0,0 +1,25 @@
+﻿using JapeCore;
+using JapeHttp;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpWebServer
+{
+    public partial class WebServer
+    {
+        private Dictionary<string, Middleware.ResponseAsync> GetResponses() => new()
+        {
+            { "/mongo", RespondMongo },
+        };
+
+        private async Task<Middleware.Result> RespondMongo(Middleware.Request request)
+        {
+            JsonData d = request.Json;
+            string result = database.MongoInsert("AjaxDB", "AjaxCol", request.Json);
+            return (Middleware.Result)await request.Complete(Status.SuccessCode.Ok, result);
+        }
+
+    }
+}
--- src/WebServer.cs
+++ src/WebServer.cs
@@ -1,73 +1,59 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JapeCore;
 using JapeDatabase;
 using JapeHttp;
 using JapeWeb;
+using Microsoft.AspNetCore.Http;
 
 namespace CSharpWebServer
 {
-    public class WebServer : JapeWeb.WebServer
+    public partial class WebServer : JapeWeb.WebServer
     {
         private const int DatabasePort = 3000;
 
-        private static string MongoConnectionString => $"mongodb+srv://admin:{MongoPassword}@cluster0.z6vns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-        private static string MongoPassword => System.Environment.GetEnvironmentVariable("API_CSHARP_WEBSERVER_MONGO");
-
-        private readonly Api databaseApi = new($"http://127.0.0.1:{DatabasePort}");
-        private Database database;
+        private static string MongoConnectionString => $"mongodb+srv://admin:{MongoPassword}@cluster0.z6vns.mongodb.net/default?retryWrites=true&w=majority";
+        private static string MongoPassword => Environment.GetEnvironmentVariable("API_CSHARP_WEBSERVER_MONGO");
 
         private readonly Templater templates;
+
+        private readonly Dictionary<string, Middleware.ResponseAsync> responses;
+
+        private DatabaseApi database;
 
         public WebServer(int http, int https) : base(http, https)
         {
             templates = CreateTemplater("./template");
+
+            responses = GetResponses();
+
+            Use(Middleware.UseAsync(Respond));
+        }
+
+        private async Task<Middleware.Result> Respond(Middleware.Request request)
+        {
+            if (!request.Path.StartsWithSegments("/request", out PathString requestPath))
+            {
+                return request.Next();
+            }
+
+            foreach (KeyValuePair<string, Middleware.ResponseAsync> response in responses)
+            {
+                if (requestPath.StartsWithSegments(response.Key))
+                {
+                     return await response.Value.Invoke(request);
+                }
+            }
+
+            return request.Next();
         }
 
         protected override async Task OnStartAsync()
         {
-            database = new(DatabasePort, 0, true, false);
+            database = new DatabaseApi(DatabasePort);
             database.UseMongo(MongoConnectionString);
-
             await database.Start();
-
-            MongoInsert("newinsertTest", "colcolcol", new JsonData
-            {
-                { "Hiya", "There" }
-            });
-        }
-
-        /// <summary>
-        /// Inserts a data into a collection.
-        /// </summary>
-        /// <param name="database">The database name.</param>
-        /// <param name="collection">The collection name.</param>
-        /// <param name="data">The data to insert.</param>
-        /// <returns>Id for the inserted data.</returns>
-        private string MongoInsert(string database, string collection, JsonData data)
-        {
-            return MongoRequest("insert", new JsonData
-            {
-                { "store", database },
-                { "collection", collection },
-                { "data", data }
-            }).Read();
-        }
-
-        private ApiResponse MongoRequest(string command, JsonData data)
-        {
-            JsonData mongoData = new(data)
-            {
-                { "index", Database.MongoIndex },
-                { "id", command }
-            };
-            return DatabaseRequest(mongoData);
-        }
-
-        private ApiResponse DatabaseRequest(JsonData data)
-        {
-            return databaseApi.BaseRequest()
-                              .SetMethod(Request.Method.Post)
-                              .WriteJson(data)
-                              .GetResponse();
         }
     }
 }
