ba201111589974975b0de4fdea86b6448aedf45c
Jonathan Paugh <j.d.paugh@gmail.com>
2021-12-09 16:07:20 -0800

    Response Updates

--- src/WebServer.Responses.cs
+++ src/WebServer.Responses.cs
@@ -1,8 +1,6 @@
 ﻿using JapeHttp;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using JapeCore;
 using JapeWeb;
 
 namespace CSharpWebServer
@@ -20,32 +18,68 @@ namespace CSharpWebServer
 
         private async Task<Middleware.Result> ResponseMongoGet(Middleware.Request request)
         {
-            string result = database.MongoGet("AjaxDB", "AjaxCol", request.Json.GetString("id"));
-            return await request.Complete(Status.SuccessCode.Ok, result);
+            try
+            {
+                JsonData result = database.MongoGet("AjaxDB", "AjaxCol", request.Json.GetString("id"));
+                return await request.Complete(Status.SuccessCode.Ok, result);
+            }
+            catch
+            {
+                return await request.Abort(Status.ErrorCode.ServerError);
+            }
+            
         }
 
         private async Task<Middleware.Result> ResponseMongoInsert(Middleware.Request request)
         {
-            string result = database.MongoInsert("AjaxDB", "AjaxCol", request.Json);
-            return await request.Complete(Status.SuccessCode.Ok, result);
+            try
+            {
+                string result = database.MongoInsert("AjaxDB", "AjaxCol", request.Json);
+                return await request.Complete(Status.SuccessCode.Ok, result);
+            }
+            catch
+            {
+                return await request.Abort(Status.ErrorCode.ServerError);
+            }
         }
 
         private async Task<Middleware.Result> ResponseMongoUpdate(Middleware.Request request)
         {
-            string result = database.MongoUpdate("AjaxDB", "AjaxCol", request.Json.GetString("id"), request.Json.Extract("data"));
-            return await request.Complete(Status.SuccessCode.Ok, result);
+            try
+            {
+                JsonData result = database.MongoUpdate("AjaxDB", "AjaxCol", request.Json.GetString("id"), request.Json.Extract("data"));
+                return await request.Complete(Status.SuccessCode.Ok, result);
+            }
+            catch
+            {
+                return await request.Abort(Status.ErrorCode.ServerError);
+            }
         }
 
         private async Task<Middleware.Result> ResponseMongoRemove(Middleware.Request request)
         {
-            string result = database.MongoRemove("AjaxDB", "AjaxCol", request.Json.GetString("id"), request.Json.GetStringArray("data").ToArray());
-            return await request.Complete(Status.SuccessCode.Ok, result);
+            try
+            {
+                JsonData result = database.MongoRemove("AjaxDB", "AjaxCol", request.Json.GetString("id"), request.Json.GetStringArray("data").ToArray());
+                return await request.Complete(Status.SuccessCode.Ok, result);
+            }
+            catch
+            {
+                return await request.Abort(Status.ErrorCode.ServerError);
+            }
         }
 
         private async Task<Middleware.Result> ResponseMongoDelete(Middleware.Request request)
         {
-            string result = database.MongoDelete("AjaxDB", "AjaxCol", request.Json.GetString("id"));
-            return await request.Complete(Status.SuccessCode.Ok, result);
+            try
+            {
+                JsonData result = database.MongoDelete("AjaxDB", "AjaxCol", request.Json.GetString("id"));
+                return await request.Complete(Status.SuccessCode.Ok, result);
+            }
+            catch
+            {
+                return await request.Abort(Status.ErrorCode.ServerError);
+            }
         }
     }
 }
